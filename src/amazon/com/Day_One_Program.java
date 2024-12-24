c497cb29d9bb2634f8065bd49d7f7e074b941afa
MaheshBmw <mahibmw99@gmail.com>
2024-12-24 11:14:09 +0530

    Update Day_One_Program.java

--- src/amazon/com/Day_One_Program.java
+++ src/amazon/com/Day_One_Program.java
@@ -3,15 +3,7 @@ package amazon.com;
 public class Day_One_Program {
 	
 	public static void main(String[] args) {
-		
-		                  //concatenation
-		System.out.println("Arthi+10"); //Should be specified inside double quotes
-		                
-		System.out.println('a');
-		
-		System.out.println(10+20);
-		
-		System.out.println(1.2);
+	
 		
 	}
 
